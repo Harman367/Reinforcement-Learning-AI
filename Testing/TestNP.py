61dc3562cd4fa48527fec3c0c2df68dd2a85e7d4
Harman Singh <harman98367@gmail.com>
2023-02-22 22:36:28 +0000

    Tested how to expand np arrays.

--- Testing/TestNP.py
+++ Testing/TestNP.py
@@ -3,7 +3,10 @@ import numpy as np
 import pandas as pd
 
 #Create Q-Table
-q_table = np.zeros((2,2))
+q_table = np.array([[1, 2], [3, 4]])
 
+print(q_table)
+
+q_table = np.lib.pad(q_table, ((0, 1), (0, 2)), 'constant', constant_values=(0))
 
 print(q_table)

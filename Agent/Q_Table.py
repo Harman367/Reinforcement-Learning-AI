b02161e4c4f8086dbf66e663cd35daaeddb192c2
Harman Singh <harman98367@gmail.com>
2023-02-22 22:37:35 +0000

    Added methods for reuseability and col/row names.

--- Agent/Q_Table.py
+++ Agent/Q_Table.py
@@ -33,42 +33,48 @@ class Q_Table:
             #print(len(self.q_actions))
             return len(self.q_actions) - 1
 
-    #Method to update the Q-Table.
-    def update(self, state, action, q_value):
-        #Get row and column for Q-Table.
+    #Method to get row and column of state and action in the Q-Table.
+    def get_row_col(self, state, action):
         row = self.get_row(state)
         col = self.get_col(action)
 
+        return row, col
+
+    #Method to expand Q-Table.
+    def expand_table(self, row, col):
         #Check if state and action is in Q-Table.
         if row >= len(self.q_table) and col >= len(self.q_table[0]):
-            self.q_table.resize((len(self.q_table) + 1, len(self.q_table[0]) + 1))
+            self.q_table = np.lib.pad(self.q_table, ((0, 1), (0, 1)), 'constant', constant_values=(0))
         
         elif row >= len(self.q_table):
-            self.q_table.resize((len(self.q_table) + 1, len(self.q_table[0])))
+            self.q_table = np.lib.pad(self.q_table, ((0, 1), (0, 0)), 'constant', constant_values=(0))
 
         elif col >= len(self.q_table[0]):
-            self.q_table.resize((len(self.q_table), len(self.q_table[0]) + 1))
+            self.q_table = np.lib.pad(self.q_table, ((0, 0), (0, 1)), 'constant', constant_values=(0))
 
+    #Method to update the Q-Table.
+    def update(self, state, action, q_value):
+        #Get row and column for Q-Table.
+        row, col = self.get_row_col(state, action)
+
+        #Expand Q-Table if needed.
+        self.expand_table(row, col)
+
+        print("State:" + str(state) + " Action:" + str(action))
         print("Value:" + str(q_value) + " added to row: " + str(row) + " col: " + str(col))
 
         #Update Q-Table.
         self.q_table[row, col] = q_value
 
+        #print(self.q_table)
+
     #Method to get Q-Value from Q-Table.
     def get_value(self, state, action):
         #Get row and column for Q-Table.
-        row = self.get_row(state)
-        col = self.get_col(action)
+        row, col = self.get_row_col(state, action)
         
-        #Check if state and action is in Q-Table.
-        if row >= len(self.q_table) and col >= len(self.q_table[0]):
-            self.q_table.resize((len(self.q_table) + 1, len(self.q_table[0]) + 1))
-        
-        elif row >= len(self.q_table):
-            self.q_table.resize((len(self.q_table) + 1, len(self.q_table[0])))
-
-        elif col >= len(self.q_table[0]):
-            self.q_table.resize((len(self.q_table), len(self.q_table[0]) + 1))
+        #Expand Q-Table if needed.
+        self.expand_table(row, col)
             
         #print(self.q_table)
         #print(row)
@@ -80,4 +86,18 @@ class Q_Table:
 
     #Method to convert Q-Table to csv file.
     def to_CSV(self):
-        pd.DataFrame(self.q_table).to_csv("Algorithm\Q-table.csv")
+        df = pd.DataFrame(self.q_table)
+
+        #Set column and row names.
+        df.columns = self.q_actions
+        df.index = self.q_states
+
+        #Print number of rows and columns.
+        #print("Number of rows: " + str(len(df.index)))
+        #print("Number of columns: " + str(len(df.columns)))
+
+        #Print number of states and actions.
+        #print("Number of states: " + str(len(self.q_states)))
+        #print("Number of actions: " + str(len(self.q_actions)))
+
+        df.to_csv("Algorithm\Q-table.csv")

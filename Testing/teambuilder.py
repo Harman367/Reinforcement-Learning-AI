5b75d562d6817311d27a9feecfa2f05c41c9c7df
Harman Singh <harman98367@gmail.com>
2023-03-23 03:04:46 +0000

    Testing

--- Testing/teambuilder.py
+++ Testing/teambuilder.py
@@ -1,5 +1,6 @@
 import asyncio
 import numpy as np
+import time
 
 from poke_env.player import RandomPlayer
 from poke_env.teambuilder import Teambuilder
@@ -14,138 +15,52 @@ class RandomTeamFromPool(Teambuilder):
 
 
 team_1 = """
-Goodra (M) @ Assault Vest
-Ability: Sap Sipper
-EVs: 248 HP / 252 SpA / 8 Spe
-Modest Nature
-IVs: 0 Atk
-- Dragon Pulse
-- Flamethrower
-- Sludge Wave
-- Thunderbolt
-
-Sylveon (M) @ Leftovers
-Ability: Pixilate
-EVs: 248 HP / 244 Def / 16 SpD
-Calm Nature
-IVs: 0 Atk
-- Hyper Voice
-- Mystical Fire
-- Protect
-- Wish
-
-Cinderace (M) @ Life Orb
-Ability: Blaze
-EVs: 252 Atk / 4 SpD / 252 Spe
-Jolly Nature
-- Pyro Ball
-- Sucker Punch
-- U-turn
-- High Jump Kick
-
-Toxtricity (M) @ Throat Spray
-Ability: Punk Rock
-EVs: 4 Atk / 252 SpA / 252 Spe
-Rash Nature
-- Overdrive
-- Boomburst
-- Shift Gear
-- Fire Punch
-
-Seismitoad (M) @ Leftovers
-Ability: Water Absorb
-EVs: 252 HP / 252 Def / 4 SpD
-Relaxed Nature
-- Stealth Rock
-- Scald
-- Earthquake
-- Toxic
-
-Corviknight (M) @ Leftovers
-Ability: Pressure
-EVs: 248 HP / 80 SpD / 180 Spe
-Impish Nature
-- Defog
-- Brave Bird
-- Roost
-- U-turn
+Roserade @ Focus Sash
+Ability: Natural Cure
+EVs: 4 Def / 252 Spd / 252 SpA
+Timid Nature
+- Grass Knot
+- HP Fire
+- Sleep Powder
+- Toxic Spikes
 """
 
 team_2 = """
-Togekiss @ Leftovers
-Ability: Serene Grace
-EVs: 248 HP / 8 SpA / 252 Spe
+Roserade @ Focus Sash
+Ability: Natural Cure
+EVs: 4 Def / 252 Spd / 252 SpA
 Timid Nature
-IVs: 0 Atk
-- Air Slash
-- Nasty Plot
-- Substitute
-- Thunder Wave
-
-Galvantula @ Focus Sash
-Ability: Compound Eyes
-EVs: 252 SpA / 4 SpD / 252 Spe
-Timid Nature
-IVs: 0 Atk
-- Sticky Web
-- Thunder Wave
-- Thunder
-- Energy Ball
-
-Cloyster @ Focus Sash
-Ability: Skill Link
-EVs: 252 Atk / 4 SpD / 252 Spe
-Adamant Nature
-- Icicle Spear
-- Rock Blast
-- Ice Shard
-- Shell Smash
-
-Sandaconda @ Focus Sash
-Ability: Sand Spit
-EVs: 252 Atk / 4 SpD / 252 Spe
-Jolly Nature
-- Stealth Rock
-- Glare
-- Earthquake
-- Rock Tomb
-
-Excadrill @ Focus Sash
-Ability: Sand Rush
-EVs: 252 Atk / 4 SpD / 252 Spe
-Adamant Nature
-- Iron Head
-- Rock Slide
-- Earthquake
-- Rapid Spin
-
-Cinccino @ Focus Sash
-Ability: Skill Link
-EVs: 252 Atk / 4 Def / 252 Spe
-Jolly Nature
-- Bullet Seed
-- Knock Off
-- Rock Blast
-- Tail Slap
+- Grass Knot
+- HP Fire
+- Sleep Powder
+- Toxic Spikes
 """
 
 custom_builder = RandomTeamFromPool([team_1, team_2])
 
 
 async def main():
+    #Time taken to run.
+    start = time.time()
+
     # We create two players
     player_1 = RandomPlayer(
-        battle_format="anythinggoes",
+        battle_format="gen4anythinggoes",
         team=custom_builder,
         max_concurrent_battles=10,
     )
     player_2 = RandomPlayer(
-        battle_format="anythinggoes",
+        battle_format="gen4anythinggoes",
         team=custom_builder,
         max_concurrent_battles=10,
     )
 
     await player_1.battle_against(player_2, n_battles=1)
+
+    # Print the results
+    battles_won = player_1.n_won_battles
+    time_taken = round(time.time() - start, 2) 
+    print(f"\nAI player won {battles_won} / {1} battles [this took {time_taken} seconds]")
 
 
 if __name__ == "__main__":
